dd2e33b04363424ad4a7f0c54ff1babed471bc1e
LawrenceVeigas <lancev.veigas3@gmail.com>
2018-02-23 09:35:44 +0530

    minor update

--- regression.py
+++ regression.py
@@ -7,8 +7,6 @@ from matplotlib import style
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 
-quandl.ApiConfig.api_key = 'qCDshF7Ksf1K47nTCSFu'
-
 df = quandl.get("WIKI/GOOGL")
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
